cb22939bb9f3917d1d15eecfed0932deb8855f9a
Eric Cugota Gomez <tryadelion@gmail.com>
2016-02-20 23:03:23 +0100

    login setted up.

--- log.php
+++ log.php
@@ -5,39 +5,29 @@ session_start();
 $uName=$_POST['stu_uName'];
 $uPass=$_POST['stu_uPass'];
 
-//$connexio=connectDB();
+$connexio=connectDB();
 
-//$uPass=sha1($uPass);
+$uPass=md5($uPass);
 
 
 if (isset($_POST['signUp'])){
   header("Location: choseSignup.php");
 }
 elseif (isset($_POST['signIn'])) {
-  $_SESSION["userName"]="Kiwi";
-  $_SESSION["userId"]="1";
-
-  header("Location: main.php");
-}
-
-
-/*
-$sql="SELECT * FROM user WHERE email='".$uName."' AND password='".$uPass."'";
-if($result = mysqli_query($connexio, $sql))
-{
-  $rowcount=mysqli_num_rows($result);
-  if($rowcount!=1){
-    header("Location: index.php?msg=1");
-  }
-  else{
-    $row = mysqli_fetch_assoc($result);
-    $_SESSION["usu"]=$row['first_name'];
-    $_SESSION["idUsu"]=$row['id'];
-    header("Location: main.php");
+  $sql="SELECT * FROM user WHERE email='".$uName."' AND password='".$uPass."'";
+  if($result = mysqli_query($connexio, $sql))
+  {
+    $rowcount=mysqli_num_rows($result);
+    if($rowcount!=1){
+      header("Location: index.php?msg=1");
+    }
+    else{
+      $row = mysqli_fetch_assoc($result);
+      $_SESSION["usu"]=$row['first_name'];
+      $_SESSION["idUsu"]=$row['id'];
+      header("Location: main.php");
+    }
   }
 }
-*/
-
-
 
 ?>
