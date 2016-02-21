675e5189a789f7cb78a4fd6a16ad9f15d5172eb5
Eric Cugota Gomez <tryadelion@gmail.com>
2016-02-21 11:25:47 +0100

    updated casespecs by damia

--- caseSpecs.php
+++ caseSpecs.php
@@ -48,7 +48,7 @@ NOM CONSELLER -> <? echo $row['tname']." ".$row['tsurname']; ?>
 ?>
 
 <!--EXEMPLE Q POTS BORRAR-->
-<div class="row">
+<!--<div class="row">
 	<div class="col s12">
 		<div class="card">
 			<div class="card-image">
@@ -64,6 +64,21 @@ NOM CONSELLER -> <? echo $row['tname']." ".$row['tsurname']; ?>
 			</div>
 		</div>
 	</div>
+</div>
+-->
+<div class="light-green left-align white-text" style="margin-right:10%; height:100%; padding:20px;" >
+		<img class="activator"   style="width: 390px; height: 250px;margin-left: 18px; float:right;" src="http://maps.google.com/maps/api/staticmap?center=<?=$row['coordX']?>,<?=$row['coordY']?>&zoom=17&size=400x300&sensor=false&markers=color:green%7C<?=$row['coordX']?>,<?=$row['coordY']?>"/>
+	<p>
+	</p> <h4>	<?=$row['title']?> </h4>
+
+	<p><h4>DESCRIPCIÓ  </h4>
+	</p>	<?=$row['description']?>
+	<p><h4>DATA  </h4>
+	</p>	<?=$row['dataIncident']?>
+	<p><h4>NOM PERSONA  </h4>
+	</p>	<? echo $row['pname']." ".$row['psurname']; ?>
+	<p><h4>NOM CONSELLER  </h4>
+	</p>	<? echo $row['tname']." ".$row['tsurname']; ?>
 </div>
 
 <?php
